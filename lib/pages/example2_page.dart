c8c6b3482d92b7c7f85b09983274ae774c8ceb08
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-30 23:07:25 -0500

    Mods

--- lib/pages/example2_page.dart
+++ lib/pages/example2_page.dart
@@ -1,4 +1,6 @@
 import 'package:codigo6_ui/widgets/item_category_widget.dart';
+import 'package:codigo6_ui/widgets/item_recommedation_widget.dart';
+import 'package:codigo6_ui/widgets/item_recommendation2_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,235 +12,18 @@ class Example2Page extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color(0xffF9FBFC),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on,
-                              size: 16.0,
-                              color: Color(0xff349DFD).withOpacity(0.5),
-                            ),
-                            const SizedBox(
-                              width: 3.0,
-                            ),
-                            Text(
-                              "Location",
-                              style: TextStyle(
-                                color: Color(0xff2B333F).withOpacity(0.5),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "Purbalingga, Jawa Tengah",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Color(0xff2B333F),
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: Stack(
-                      children: [
-                        Icon(
-                          Icons.notifications,
-                          size: 24.0,
-                          color: Color(0xffC6C6C6),
-                        ),
-                        Positioned(
-                          top: 3,
-                          right: 2,
-                          child: Container(
-                            width: 10,
-                            height: 10,
-                            decoration: BoxDecoration(
-                              color: Colors.redAccent,
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: Colors.white,
-                                width: 1.7,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.04),
-                      blurRadius: 12,
-                      offset: const Offset(4, 4),
-                    ),
-                  ],
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: "Search",
-                    suffixIcon: Icon(
-                      Icons.search,
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 18,
-                      vertical: 14.0,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14.0),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              const Text(
-                "Category",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 14.0,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ItemCategoryWidget(
-                      icon: "home",
-                      text: "House",
-                      isSelected: true,
-                    ),
-                    ItemCategoryWidget(
-                      icon: "city",
-                      text: "Hotel",
-                      isSelected: false,
-                    ),
-                    ItemCategoryWidget(
-                      icon: "house",
-                      text: "Apartment",
-                      isSelected: false,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              const Text(
-                "Recommendation",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(14.0),
-                constraints: BoxConstraints(
-                  maxWidth: width * 0.6,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.04),
-                      blurRadius: 12,
-                      offset: const Offset(4, 4),
-                    ),
-                  ],
-                ),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    //Imagen principal
-                    Container(
-                      width: width * 0.6,
-                      height: height * 0.2,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16.0),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    //Título y puntuación
-                    const SizedBox(
-                      height: 14.0,
-                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Expanded(
-                          child: Text(
-                            "Minimalist House",
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w600,
-                              height: 1.2,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: const [
-                            Icon(
-                              Icons.star,
-                              size: 16.0,
-                              color: Colors.amber,
-                            ),
-                            Text(
-                              "4.5",
-                              style: TextStyle(
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-
-                    Row(
                       children: [
                         Expanded(
                           child: Column(
@@ -246,55 +31,211 @@ class Example2Page extends StatelessWidget {
                             children: [
                               Row(
                                 children: [
-                                  Text(
-                                    "\$734",
-                                  ),
-                                  Text(
-                                    "/Month",
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
                                   Icon(
                                     Icons.location_on,
-                                    size: 14.0,
+                                    size: 16.0,
+                                    color: Color(0xff349DFD).withOpacity(0.5),
                                   ),
-                                  Expanded(
-                                    child: Text(
-                                      "Padamara, Jawa Tengah",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                        height: 1.1,
-                                      ),
+                                  const SizedBox(
+                                    width: 3.0,
+                                  ),
+                                  Text(
+                                    "Location",
+                                    style: TextStyle(
+                                      color: Color(0xff2B333F).withOpacity(0.5),
                                     ),
                                   ),
                                 ],
+                              ),
+                              Text(
+                                "Purbalingga, Jawa Tengah",
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  color: Color(0xff2B333F),
+                                  fontWeight: FontWeight.w700,
+                                ),
                               ),
                             ],
                           ),
                         ),
                         Container(
-                          padding: const EdgeInsets.all(6.0),
+                          padding: const EdgeInsets.all(12.0),
                           decoration: BoxDecoration(
-                            color: Color(0xffF8F8F8),
-                            borderRadius: BorderRadius.circular(8.0),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                          child: Icon(
-                            Icons.bookmark_rounded,
-                            color: Color(0xff89909A),
+                          child: Stack(
+                            children: [
+                              Icon(
+                                Icons.notifications,
+                                size: 24.0,
+                                color: Color(0xffC6C6C6),
+                              ),
+                              Positioned(
+                                top: 3,
+                                right: 2,
+                                child: Container(
+                                  width: 10,
+                                  height: 10,
+                                  decoration: BoxDecoration(
+                                    color: Colors.redAccent,
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: Colors.white,
+                                      width: 1.7,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.04),
+                            blurRadius: 12,
+                            offset: const Offset(4, 4),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: "Search",
+                          suffixIcon: Icon(
+                            Icons.search,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 18,
+                            vertical: 14.0,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14.0),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    const Text(
+                      "Category",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 14.0,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          ItemCategoryWidget(
+                            icon: "home",
+                            text: "House",
+                            isSelected: true,
+                          ),
+                          ItemCategoryWidget(
+                            icon: "city",
+                            text: "Hotel",
+                            isSelected: false,
+                          ),
+                          ItemCategoryWidget(
+                            icon: "house",
+                            text: "Apartment",
+                            isSelected: false,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    const Text(
+                      "Recommendation",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      physics: const BouncingScrollPhysics(),
+                      child: Row(
+                        children: [
+                          ItemRecommendationWidget(),
+                          ItemRecommendationWidget(),
+                          ItemRecommendationWidget(),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    const Text(
+                      "Recommendation",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    ItemRecommnedation2Widget(),
+                    ItemRecommnedation2Widget(),
+                    ItemRecommnedation2Widget(),
+                    ItemRecommnedation2Widget(),
+                    ItemRecommnedation2Widget(),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.home,
+                  ),
+                  Icon(
+                    Icons.home,
+                  ),
+                  Icon(
+                    Icons.home,
+                  ),
+                  Icon(
+                    Icons.home,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
