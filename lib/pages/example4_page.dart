84be613ab41e87202513cd2379556f8114f9da33
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-31 22:42:10 -0500

    Mods

--- lib/pages/example4_page.dart
+++ lib/pages/example4_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Example4Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Colors.amber,
         image: DecorationImage(
           fit: BoxFit.cover,
@@ -21,10 +22,91 @@ class Example4Page extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Column(
-          children: [
-            TextField(),
-          ],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Center(
+                  child: Text(
+                    "Burger \nQueen",
+                    style: GoogleFonts.fredokaOne(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      height: 1,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Expanded(child: SizedBox()),
+                Text(
+                  "Good Food",
+                  style: GoogleFonts.fredokaOne(
+                    color: Colors.white,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w800,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Color(0xffFF8D07).withOpacity(0.5),
+                    decorationThickness: 2.5,
+                  ),
+                ),
+                Text(
+                  "Tasty Food *",
+                  style: GoogleFonts.fredokaOne(
+                    color: Colors.white,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w800,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Color(0xffFF8D07).withOpacity(0.5),
+                    decorationThickness: 2.5,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 54.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign in",
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffFF8D07),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
